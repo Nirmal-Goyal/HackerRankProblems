c9e0a6a4f0ba8e6d3f22c5051505b204ae8b145a
Nirmal <nirmalgoyal0108@gmail.com>
2025-02-07 12:47:22 +0530

    added swap program in C

---
+++ SwapingTwoNumber.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+
+    int a, b;
+    printf("numbers before swap: ");
+    scanf("%d %d", &a, &b);
+    
+    // swaping of two number using third variable
+    /*
+    int temp=a;
+    a=b;
+    b=temp;
+    */
+
+    // swaping of number without using third variable
+    a = a+b;
+    b = a-b;
+    a = a-b;
+    
+    printf("numbres after swap: %d %d", a, b);
+    return 0;
+}
