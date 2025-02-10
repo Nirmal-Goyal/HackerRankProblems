3090b391324c7a8cdd0441e291557b914bd80745
Nirmal <nirmalgoyal0108@gmail.com>
2025-02-11 00:03:16 +0530

    Print all even numbers from 1 to N

--- Printing-N-EvenNumbers.c
+++ Printing-N-EvenNumbers.c
@@ -6,8 +6,18 @@ int main() {
     int N;
     printf("Enter the last number: ");
     scanf("%d", &N);
+    //Method 1 without using if-statement
+    /*
     for(int i=2; i<=N; i+=2){
         printf("%d ", i);
+    }
+    */
+
+    // Method 2 using if-statement
+    for(int i=1; i<=N; i++){
+        if(i%2==0){
+            printf("%d ", i);
+        }
     }
     return 0;
 }
