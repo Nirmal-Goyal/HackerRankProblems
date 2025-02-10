2de6aeda44eae208aa36e6060b9ecc67f1c9a00b
Nirmal <nirmalgoyal0108@gmail.com>
2025-02-10 23:50:33 +0530

    Print all natural numbers

---
+++ Printing-N-NaturalNumbers.c
@@ -0,0 +1,13 @@
+// Print all natural numbers from 1 to N
+#include <stdio.h>
+
+int main() {
+   
+    int N;
+    scanf("%d", &N);
+    for(int i=1; i<=N; i++){
+        printf("%d ", i);
+    }
+    
+    return 0;
+}
