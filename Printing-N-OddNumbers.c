320173f06ac92500acb380cb357717201fd8442e
Nirmal <nirmalgoyal0108@gmail.com>
2025-02-11 22:44:11 +0530

    all odd numbers from 1 to N

--- Printing-N-OddNumbers.c
+++ Printing-N-OddNumbers.c
@@ -6,6 +6,17 @@ int main() {
     int N;
     printf("enter the last number: ");
     scanf("%d", &N);
+
+    // Method-1 using if-statement
+    /*
+    for(int i=1; i<=N; i++){
+        if(i%2==1){
+            printf("%d ", i);
+        }
+    }
+    */
+
+    // Method-2 without if-statement
     for(int i=1; i<=N; i+=2){
         printf("%d ", i);
     }
