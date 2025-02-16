0c7d2ec07b9a40ed8566982dbd3a67871634dcc9
Nirmal <nirmalgoyal0108@gmail.com>
2025-02-16 13:11:19 +0530

    Pattern_Square

---
+++ PrintingSquare.c
@@ -0,0 +1,18 @@
+// Pattern_Square with *
+#include <stdio.h>
+
+int main() {
+
+    int n;
+    printf("enter the num: ");
+    scanf("%d", &n);
+    
+    for(int i=1; i<=n; i++){
+        for(int j=1; j<=n; j++){
+            printf("* ");
+        }
+        printf("\n");
+    }
+    
+    return 0;
+}
