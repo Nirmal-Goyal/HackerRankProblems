4aafaa81f2eb9a350c12cf5967537f7a493154c8
Nirmal <nirmalgoyal0108@gmail.com>
2025-03-02 00:37:33 +0530

    sum of prime factors

--- Prime_Factors_Sum.c
+++ Prime_Factors_Sum.c
@@ -4,18 +4,21 @@
 int main() {
 
     int n;
+    printf("enter the num: ");
     scanf("%d", &n);
+    // initialized the sum of prime factors with 0
     int sum = 0;
+    // prime factors start from '2'
     int i=2;
     while(n!=1){
         if(n%i==0){
             sum = sum + i;
-            n = n/i;
+            n = n/i;  // divide with same prime factor
         }
         else{
             ++i;
         }
     }
-    printf("%d", sum);
+    printf("sum of all prime factors is: %d", sum);
     return 0;
 }
