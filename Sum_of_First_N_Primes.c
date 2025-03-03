7fb3d6141f3eeff6962f8621b6df1465354067a9
Nirmal <nirmalgoyal0108@gmail.com>
2025-03-03 12:12:26 +0530

    sum of first n primes

---
+++ Sum_of_First_N_Primes.c
@@ -0,0 +1,31 @@
+// Print a single integer representing the sum of the first N prime numbers.
+#include <stdio.h>
+
+int main() {
+
+    int n;
+    scanf("%d", &n);
+    int count=0;
+    int num=2;
+    int sum=0;
+  
+    while(count<=n){
+        int prime=1;
+        for(int i=2; i*i<=num; i++){
+            if(num%i==0){
+                prime=0;
+                break;
+            }
+        }
+        if(prime==1){
+            count++;
+            sum = sum+num;
+        }
+        if(count==n){
+            printf("%d", sum);
+            break;
+        }
+        num++;
+    }   
+    return 0;
+}
