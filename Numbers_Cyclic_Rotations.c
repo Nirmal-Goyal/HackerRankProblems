f2f3cc88ac18ae5528723e08e3528a49c92bea8d
Nirmal <nirmalgoyal0108@gmail.com>
2025-03-06 12:43:56 +0530

    cyclic rotation

--- Numbers_Cyclic_Rotations.c
+++ Numbers_Cyclic_Rotations.c
@@ -1,10 +1,11 @@
 // Print all cyclic rotations of N, one per line, in the order they are obtained by shifting the digit in the one's place to the left. The process stops when the number returns to its original configuration.
 #include <stdio.h>
 int main() {
-    int n,count=0,num;  
+    int n;
+    printf("enter the num: ");  
     scanf("%d",&n);
-    
-    int x=1;
+
+    int count=0, x=1;
     int a=n;
     while(a>0)
     {
@@ -13,11 +14,13 @@ int main() {
         x=x*10;
     }
     x=x/10;
-    
-    for(int i=1; i<=count; i++)
-    {
-        num=(n%10)*x+n/10;
-        printf("%d\n",num);
+
+    int num;
+
+    printf("cyclic rotation:\n");
+    for(int i=1; i<=count; i++){
+        num = (n%10)*x + (n/10);
+        printf("%d\n", num);
         n=num;
-    }
+    }   
 }
