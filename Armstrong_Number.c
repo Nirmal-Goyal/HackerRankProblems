90564148e1a6c0392691d028f1b00e91f902ae98
Nirmal <nirmalgoyal0108@gmail.com>
2025-03-09 13:35:58 +0530

    armstrong number

--- Armstrong_Number.c
+++ Armstrong_Number.c
@@ -1,21 +1,18 @@
 // An Amrstrong number is a number that is equal to the sum of its own digits each raised to the power of the number of digits
 // Print "YES" if the number is an Armstrong number, otherwise "NO"
 #include <stdio.h>
+#include<math.h>
 
 int main() {
     int n;
+    printf("enter the num: ");
     scanf("%d", &n);
-    int count=0; 
-    int a=n;
+
+    int count = log10(n)+1; 
     int original=n;     
     int sum=0;
     
-    while(a>0){
-        ++count;
-        a=a/10;
-    }
-    
-    for(int i=1; i<=count; i++){
+    while(n>0){
         int rem = n%10;
         int product=1;
         for(int j=1; j<=count; j++){
@@ -25,12 +22,10 @@ int main() {
         n=n/10;
     }
     
-    if(sum==original){
+    if(sum==original)
         printf("YES");
-    }
-    else{
+    else
         printf("NO");
-    }
     
     return 0;
 }
