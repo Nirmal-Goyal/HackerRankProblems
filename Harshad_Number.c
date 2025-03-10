2728ca3c7b1cb86ef37dab9607128f3d73ae0740
Nirmal <nirmalgoyal0108@gmail.com>
2025-03-10 09:41:20 +0530

    Harshad Number

---
+++ Harshad_Number.c
@@ -0,0 +1,23 @@
+// A Harshad Number is a number that is divisible by the sum of its digits.
+// Print weather number is Harshad or not?
+#include <stdio.h>
+
+int main() {
+
+    int n;
+    scanf("%d", &n);
+    int original = n;
+    int sum=0;
+    
+    while(n>0){
+        sum = sum + n%10;
+        n = n/10;
+    }
+    if(original%sum==0){
+        printf("%d is a harshad number", original);
+    }
+    else{
+        printf("%d is not a harshad number", original);
+    }
+    return 0;
+}
