ff373df466bb0d7529b20677ffa473c33b961b9b
Nirmal <nirmalgoyal0108@gmail.com>
2025-03-22 21:43:56 +0530

    missing number

--- Missing_Number_in_an_Array.c
+++ Missing_Number_in_an_Array.c
@@ -2,13 +2,16 @@
 #include <stdio.h>
 int main() {  
     int n;
+    printf("enter the actual size of array: ");
     scanf("%d", &n);
-    int arr[n-1];
+    int arr[n-1]; // declare the array of n-1 size
     int sum=0;
+    printf("enter the element of array: ");
     for(int i=0; i<=n-2; i++){
         scanf("%d", &arr[i]);
         sum+=arr[i];
     }
-    printf("%d", (n*(n+1))/2 - sum);
+    // missing element will be sum of first n elements - sum of elements of array
+    printf("missing element is: %d", (n*(n+1))/2 - sum);
     return 0;
 }
